0b6ee1a2cc1214cf162842af8a138f6c40cbc23e
Justin Byrne <theskaterjesus@gmail.com>
2021-08-18 08:15:53 +0100

    display image on the index page

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -41,7 +41,9 @@
                                                 <tr>
                                                     <td class="px-6 py-4 whitespace-nowrap">
                                                         <div class="flex-shrink-0 h-10 w-10">
-                                                            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
+                                                            @if ($article->getMedia()->count())
+                                                                <img class="h-10 w-10 object-cover" src="{{ $article->getFirstMediaUrl() }}" alt="">
+                                                            @endif
                                                         </div>
                                                     </td>
                                                     <td class="px-6 py-4 whitespace-nowrap">

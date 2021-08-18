1379061e4cacec286751f62c23bed4a472390131
Justin Byrne <theskaterjesus@gmail.com>
2021-08-18 08:42:21 +0100

    Updated and added buttons

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex justify-end pb-3">
-                        <a href="{{ route('articles.create') }}" class="shadow rounded-lg px-3 py-2 bg-gray-50">
+                        <a href="{{ route('articles.create') }}" class="shadow rounded-lg px-3 py-2 text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             New Article
                         </a>
                     </div>
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    
+                    <div class="flex justify-end pb-3">
+                        <a href="{{ route('categories.create') }}" class="shadow rounded-lg px-3 py-2 text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            New Category
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>

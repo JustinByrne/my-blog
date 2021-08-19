7b75db7d4cdec62460c140af82b3b802f75b5150
Justin Byrne <theskaterjesus@gmail.com>
2021-08-19 20:25:07 +0100

    Updated/added slug to create view

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -11,7 +11,7 @@
                 <form action="{{ route('categories.store') }}" method="POST">
                     @csrf
                     <div class="p-6 bg-white border-b border-gray-200">
-                        @livewire('category.slug')
+                        @livewire('slug')
                     </div>
                     <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                         <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">

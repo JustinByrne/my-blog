1ac504d9a1ebacf275fdf77fdbc26db40d8c873c
Justin Byrne <theskaterjesus@gmail.com>
2021-08-19 20:25:26 +0100

    Edited view to include form and slug component

--- resources/views/tag/edit.blade.php
+++ resources/views/tag/edit.blade.php
@@ -8,9 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    
-                </div>
+                <form action="{{ route('categories.update', $tag) }}" method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        @livewire('slug', ['model' => $tag])
+                    </div>
+                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Save
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>

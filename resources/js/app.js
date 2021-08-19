def4fdd1c158bbc38268f57c33ff3cd2691cc9e8
Justin Byrne <theskaterjesus@gmail.com>
2021-08-19 22:03:36 +0100

    added ckeditor5

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+ClassicEditor
+    .create(document.querySelector('#editor'))
+    .then(editor => {
+        console.log(editor);
+    })
+    .catch(error => {
+        console.error(error);
+    });

7a0a926c86bcfd4c3a3255f81dc2e470c1ff46e7
Justin Byrne <theskaterjesus@gmail.com>
2021-08-19 20:24:47 +0100

    Moved slug to reusable and added mount

--- app/Http/Livewire/Slug.php
+++ app/Http/Livewire/Slug.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire\Category;
+namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Str;
@@ -10,9 +10,17 @@ class Slug extends Component
     public $name;
     public $slug;
     
+    public function mount($model = null)
+    {
+        if (! is_null($model)) {
+            $this->name = $model->name;
+            $this->slug = $model->slug;
+        }
+    }
+    
     public function render()
     {
-        return view('livewire.category.slug');
+        return view('livewire.slug');
     }
 
     public function updatedName()

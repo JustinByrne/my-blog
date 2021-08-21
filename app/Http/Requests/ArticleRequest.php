3c9e424447fc1157c2f7500012a727431333802f
Justin Byrne <theskaterjesus@gmail.com>
2021-08-21 11:17:20 +0100

    Added category id to request

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -26,6 +26,7 @@ class ArticleRequest extends FormRequest
         return [
             'title' => 'required',
             'content' => 'required',
+            'category_id' => 'required',
         ];
     }
 }

6557e05b805cf3924da917e00df1c7e8c7b55dad
Justin Byrne <theskaterjesus@gmail.com>
2021-08-21 23:04:57 +0100

    fixed edit page

--- resources/views/tag/edit.blade.php
+++ resources/views/tag/edit.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <form action="{{ route('categories.update', $tag) }}" method="POST">
+                <form action="{{ route('tags.update', $tag) }}" method="POST">
                     @csrf
                     @method('PATCH')
                     <div class="p-6 bg-white border-b border-gray-200">

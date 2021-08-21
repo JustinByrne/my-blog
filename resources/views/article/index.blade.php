49b91483bbe4889dd763dd6328518ae2411362b7
Justin Byrne <theskaterjesus@gmail.com>
2021-08-21 11:52:46 +0100

    Changed colour to match scheme

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -63,7 +63,7 @@
                                                     </td>
                                                     <td class="px-6 py-4 whitespace-nowrap">
                                                         @foreach ($article->tags as $tag)
-                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-indigo-100 text-indigo-800">
                                                                 {{ $tag->name }}
                                                             </span>
                                                         @endforeach

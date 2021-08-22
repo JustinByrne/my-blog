715eb17f68ab6059abf776b1dd1a1ae9d54cdf2e
Justin Byrne <theskaterjesus@gmail.com>
2021-08-22 17:45:29 +0100

    Added filepond

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,6 @@ require('alpinejs');
 
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 window.ClassicEditor = ClassicEditor;
+
+import * as FilePond from 'filepond';
+window.FilePond = FilePond;

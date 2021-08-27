ac805e126378445d0fe1d51ed5553bf1a2bd24b8
Justin Byrne <theskaterjesus@gmail.com>
2021-08-27 21:07:43 +0100

    Removed excess comments

--- public/js/ckeditor.js
+++ public/js/ckeditor.js
@@ -99,25 +99,7 @@ _ckeditor_ckeditor5_build_classic__WEBPACK_IMPORTED_MODULE_0___default().create(
   console.log(editor);
 })["catch"](function (error) {
   console.error(error);
-}); // import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-// import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-// import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-// import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-// import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-// import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-// import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
-// import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-// ClassicEditor
-//     .create(document.querySelector('#editor'), {
-//         plugins: [Essentials, Paragraph, Bold, Italic, Alignment, CodeBlock, BlockQuote],
-//         toolbar: ['bold', 'italic', 'alignment', 'codeblock']
-//     })
-//     .then(editor => {
-//         console.log('Editor was initialized', editor);
-//     })
-//     .catch(error => {
-//         console.error(error.stack);
-//     });
+});
 })();
 
 /******/ })()

55ba004f909307b79a94af60dcd378ed19b8d853
Justin Byrne <theskaterjesus@gmail.com>
2021-08-28 19:48:24 +0100

    Changed styling of the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,28 +2,28 @@
     <x-slot name="title">
         {{ config('app.name', 'Laravel') }}
     </x-slot>
-    <div class="flex flex-col space-y-8 px-4 lg:px-40 py-4">
+    <div class="flex flex-col space-y-20 px-4 lg:px-40 py-4">
         @foreach ($articles as $article)
-            <div class="grid grid-cols-1 lg:grid-cols-4">
+            <div class="grid grid-cols-1 lg:grid-cols-5 gap-8 h-56">
                 <div
                     @class([
+                        'lg:col-span-2',
                         'lg:order-last' => $loop->odd,
                     ])
                 >
                     @if ($article->getMedia()->count())
-                        <img src="{{ $article->getFirstMediaUrl() }}" class="rounded-md shadow-md w-56 h-32 object-cover">
+                        <img src="{{ $article->getFirstMediaUrl() }}" class="rounded-md shadow-md w-full object-cover mt-0">
                     @endif
                 </div>
                 <div
                     @class([
-                        'lg:col-span-3',
-                        'lg:text-right' => $loop->even,
+                        'lg:col-span-3 prose prose-indigo min-w-full',
                     ])
                 >
                     <h2>
                         {{ $article->title }}
                     </h2>
-                    <div class="prose min-w-full">
+                    <div>
                         {!! \Illuminate\Support\Str::words($article->content, 40, '...') !!}
                     </div>
                 </div>

eefce1ec262085a0cdd3abc172458e161154c9f7
Justin Byrne <theskaterjesus@gmail.com>
2021-09-04 15:51:08 +0100

    Improved ability to create new categories

--- app/Http/Livewire/Category.php
+++ app/Http/Livewire/Category.php
@@ -3,17 +3,29 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Str;
 use App\Models\Category as Categories;
 
 class Category extends Component
 {
     public $category;
     public $categories;
+    public $selectedCategory;
+    public $newCategory;
+
+    protected $rules = [
+        'newCategory' => [
+            'required',
+            'unique:App\Models\Category,name'
+        ],
+    ];
     
     public function mount($article = null)
     {
+        $this->selectedCategory = 1;
+        
         if (! is_null($article)) {
-            $this->public = $article->category;
+            $this->selectedCategory = $article->category->id;
         }
     }
     
@@ -22,5 +34,23 @@ class Category extends Component
         $this->categories = Categories::all();
         
         return view('livewire.category');
+    }
+
+    public function changeCategory($id) {
+        $this->selectedCategory = $id;
+    }
+
+    public function addCategory()
+    {
+        $this->validate();
+
+        $slug = Str::slug($this->newCategory);
+
+        Categories::create([
+            'name' => $this->newCategory,
+            'slug' => $slug,
+        ]);
+
+        $this->newCategory = null;
     }
 }
--- resources/views/livewire/category.blade.php
+++ resources/views/livewire/category.blade.php
@@ -2,26 +2,31 @@
     <label for="category_id" class="block text-sm font-medium text-gray-700">
         Category <span class="text-red-400">*</span>
     </label>
-    <select
-        name="category_id"
-        @class([
-            'mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm rounded-md',
-            'border-gray-300' => ! $errors->has('category'),
-            'border-red-400' => $errors->has('category'),
-        ])
-        required="required"
-    >
-        @foreach ($categories as $category)
-            <option
-                value="{{ $category->id }}"
-                @if (old('category_id') == $category->id)
-                    selected="selected"
-                @elseif ($category->id == 1)
-                    selected="selected"
-                @endif
-            >
-                {{ $category->name }}
-            </option>
-        @endforeach
-    </select>
+    <div class="flex flex-col mt-1 h-36 border border-gray-300 rounded-md">
+        <div class="flex flex-col mb-auto px-2 py-1 overflow-y-auto">
+            @foreach ($categories as $category)
+                <label class="inline-flex items-center">
+                    <input
+                        type="checkbox"
+                        class="form-checkbox h-4 w-4 text-red-600 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                        value="{{ $category->id }}"
+                        @if ($selectedCategory == $category->id)
+                            checked
+                        @endif
+                        wire:click="changeCategory({{ $category->id }})"
+                    >
+                    <span class="ml-1 text-gray-700">
+                        {{ $category->name }}
+                    </span>
+                </label>
+            @endforeach
+        </div>
+        <div class="relative mt-1">
+            <input type="text" wire:model="newCategory" class="focus:outline-none focus:ring-2 focus:ring-red-500 block w-full sm:text-sm border-0 border-t border-gray-300 rounded-b-md" placeholder="Add Category">
+            <button wire:click="addCategory" type="button" class="absolute origin-top-right top-0 right-0 h-full w-10 bg-red-600 hover:bg-red-700 rounded-br-md font-semibold text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                +
+            </button>
+        </div>
+        <input type="hidden" name="category_id" wire:model="selectedCategory">
+    </div>
 </div>

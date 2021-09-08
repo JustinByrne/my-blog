52f48081ccdefa96eded8d11d1ef0ca9b02ed2bd
Justin Byrne <theskaterjesus@gmail.com>
2021-09-08 16:52:25 +0100

    Added a filepond upload for new images

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -9,11 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    {{-- <div class="flex justify-end pb-3">
-                        <a href="{{ route('articles.create') }}" class="shadow rounded-lg px-3 py-2 text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                            Upload new Image
-                        </a>
-                    </div> --}}
+                    <div class="flex justify-end pb-4">
+                        <div class="w-full" x-data x-init="getImg($refs.input)">
+                            <input type="file" name="image" id="image" x-ref="input">
+                        </div>
+                    </div>
                     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                         @foreach ($media as $image)
                             <img class="w-full max-h-36 object-cover" src="{{ $image->getFullUrl() }}" alt="{{ $image->name }}">
@@ -23,4 +23,18 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function getImg(element) {
+            FilePond.create(element);
+            FilePond.setOptions({
+                server: {
+                    url: '{{ route('media.upload') }}',
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                    }
+                }
+            });
+        }
+    </script>
 </x-app-layout>

a7aa285cde97e65feb8fe11d8e4d7893568bc3c7
Justin Byrne <theskaterjesus@gmail.com>
2021-09-12 16:11:27 +0100

    Improved how the image popup shows

--- resources/views/livewire/images.blade.php
+++ resources/views/livewire/images.blade.php
@@ -7,24 +7,25 @@
 
     <div
         @class([
-            'absolute inset-0 bg-gray-900 bg-opacity-40',
+            'fixed inset-0 bg-gray-900 bg-opacity-40 cursor-pointer',
             'hidden' => is_null($selectedImage)
         ])
+        wire:click="closeImage"
     ></div>
 
     <div
         @class([
-            'absolute origin-top-left bg-white p-4 rounded-md shadow-lg top-4 left-4 right-4 grid lg:grid-cols-3 gap-4',
+            'fixed transform top-20 lg:top-1/2 lg:-translate-y-1/2 inset-x-4 lg:max-w-screen-lg lg:mx-auto lg:max-h-screen bg-white p-4 rounded-lg grid lg:grid-cols-3 gap-4',
             'hidden' => is_null($selectedImage)
         ])
     >
-        <div class="lg:col-span-2">
-            <img class="w-full" src="{{ $selectedImage }}">
-        </div>
-        <div>
+        <div class="lg:order-last">
             <div class="flex justify-end">
                 <span class="cursor-pointer" wire:click="closeImage">X</span>
             </div>
+        </div>
+        <div class="lg:col-span-2 aspect-w-1 aspect-h-1">
+            <img class="w-full object-contain" src="{{ $selectedImage }}">
         </div>
     </div>
 </div>

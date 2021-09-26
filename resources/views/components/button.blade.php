ecaed5ac6763d3da675ca37530c8091c3135a340
Justin Byrne <theskaterjesus@gmail.com>
2021-09-26 19:57:30 +0100

    Updated the button to include a secondary version

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,15 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props(['secondary'])
+
+@php
+    $class = 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:border-red-900 focus:ring disabled:opacity-25 transition ease-in-out duration-150';
+
+    if (! isset($secondary)){
+        $class .= ' bg-red-600 hover:bg-red-500 active:bg-red-900 ring-red-300 focus:border-red-900';
+    } else {
+        $class .= ' bg-gray-800 hover:bg-gray-700 active:bg-gray-900 ring-gray-300 focus:border-gray-900';
+    }
+@endphp
+
+<button {{ $attributes->merge(['type' => 'submit', 'class' => $class]) }}>
     {{ $slot }}
 </button>

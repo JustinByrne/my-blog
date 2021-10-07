149c879fc00c820b5b834d3b3414688bb12737e0
Justin Byrne <theskaterjesus@gmail.com>
2021-10-07 21:11:01 +0100

    Created a settings store function #3

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Setting;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use App\Http\Requests\SettingRequest;
+use Illuminate\Http\RedirectResponse;
 
 class SettingsController extends Controller
 {
@@ -13,5 +15,13 @@ class SettingsController extends Controller
         $settings = Setting::all();
         
         return view('settings.general', compact('settings'));
+    }
+
+    public function storeSettings(SettingRequest $request)
+    {
+        Setting::where('name', 'site_name')->first()->update(['value' => $request->site_name]);
+        Setting::where('name', 'tag_line')->first()->update(['value' => $request->tag_line]);
+
+        return redirect()->route('settings.general');
     }
 }

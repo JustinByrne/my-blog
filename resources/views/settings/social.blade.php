b2455243c38bb7708117035cad43f8c51bbd6470
Justin Byrne <theskaterjesus@gmail.com>
2021-10-08 09:17:26 +0100

    Added social form view #4

---
+++ resources/views/settings/social.blade.php
@@ -0,0 +1,53 @@
+<x-setting-layout>
+    <x-slot name="header">
+        Social Media
+    </x-slot>
+
+    <form action="{{ route('settings.storeSocial') }}" method="POST">
+        @csrf
+        @method('PATCH')
+        <div class="shadow sm:rounded-md sm:overflow-hidden">
+            <div class="bg-white py-6 px-4 sm:p-6">
+                <div class="grid grid-cols-1 gap-y-6 sm:grid-cols-6">
+                    <div class="sm:col-span-6">
+                        <label for="facebook_url" class="block text-sm font-medium text-gray-700">
+                            Facebook Link
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <x-input type="text" name="facebook_url" id="facebook_url" class="w-full" :value="old('facebook_url', $settings->where('name', 'facebook_url')->first()->value)" placeholder="https://" />
+                        </div>
+                    </div>
+                    <div class="sm:col-span-6">
+                        <label for="instagram_url" class="block text-sm font-medium text-gray-700">
+                            Instagram Link
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <x-input type="text" name="instagram_url" id="instagram_url" class="w-full" :value="old('instagram_url', $settings->where('name', 'instagram_url')->first()->value)" placeholder="https://" />
+                        </div>
+                    </div>
+                    <div class="sm:col-span-6">
+                        <label for="github_url" class="block text-sm font-medium text-gray-700">
+                            GitHub Link
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <x-input type="text" name="github_url" id="github_url" class="w-full" :value="old('github_url', $settings->where('name', 'github_url')->first()->value)" placeholder="https://" />
+                        </div>
+                    </div>
+                    <div class="sm:col-span-6">
+                        <label for="twitter_url" class="block text-sm font-medium text-gray-700">
+                            Twitter Link
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <x-input type="text" name="twitter_url" id="twitter_url" class="w-full" :value="old('twitter_url', $settings->where('name', 'twitter_url')->first()->value)" placeholder="https://" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <x-button>
+                    Save
+                </x-button>
+            </div>
+        </div>
+    </form>
+</x-setting-layout>

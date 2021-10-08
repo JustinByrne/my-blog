7b25adfab737eb39e90b77472afd3d20552dfdd0
Justin Byrne <theskaterjesus@gmail.com>
2021-10-08 09:13:47 +0100

    Included the social media settings on the page #4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         View::share('site_name', Setting::where('name', 'site_name')->first()->value);
+        View::share('tag_line', Setting::where('name', 'tag_line')->first()->value);
+        View::share('facebook_url', Setting::where('name', 'facebook_url')->first()->value);
+        View::share('instagram_url', Setting::where('name', 'instagram_url')->first()->value);
+        View::share('github_url', Setting::where('name', 'github_url')->first()->value);
+        View::share('twitter_url', Setting::where('name', 'twitter_url')->first()->value);
     }
 }

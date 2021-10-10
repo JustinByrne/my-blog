5acbb19f5aa992b5d839b257bf2f53fc2c357e18
Justin Byrne <theskaterjesus@gmail.com>
2021-10-10 18:24:33 +0100

    Added site logo to all views #5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         View::share('site_name', Setting::where('name', 'site_name')->first()->value);
         View::share('tag_line', Setting::where('name', 'tag_line')->first()->value);
+        View::share('site_logo', Setting::where('name', 'site_logo')->first()->value);
         View::share('facebook_url', Setting::where('name', 'facebook_url')->first()->value);
         View::share('instagram_url', Setting::where('name', 'instagram_url')->first()->value);
         View::share('github_url', Setting::where('name', 'github_url')->first()->value);

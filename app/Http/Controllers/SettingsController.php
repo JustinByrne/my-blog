5fb063d6f470e1a1ddd4e8b18f5ddb3647157d6c
Justin Byrne <theskaterjesus@gmail.com>
2021-10-10 18:22:49 +0100

    Added ability to update table with image url #5

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use App\Models\Setting;
+use App\Models\TempFile;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use App\Http\Requests\SocialRequest;
 use App\Http\Requests\SettingRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 
 class SettingsController extends Controller
 {
@@ -22,6 +25,14 @@ class SettingsController extends Controller
     {
         Setting::where('name', 'site_name')->first()->update(['value' => $request->site_name]);
         Setting::where('name', 'tag_line')->first()->update(['value' => $request->tag_line]);
+
+        $file = TempFile::where('folder', $request->image)->first();
+        if ($file) {
+            $media = Image::find(1);
+            $image = $media->addMedia(Storage::path($request->image . '/' . $file->filename))->toMediaCollection();
+            Setting::where('name', 'site_logo')->first()->update(['value' => $image->getUrl()]);
+            $file->delete();
+        }
 
         return redirect()->route('settings.general');
     }

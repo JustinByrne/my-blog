8c0aea6420d657387dcf6f8c09ee2a54d2e19d32
Justin Byrne <theskaterjesus@gmail.com>
2021-10-16 23:25:20 +0100

    Added requirements to the setup page

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -4,11 +4,59 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class SetupController extends Controller
 {
     public function index(): View
     {
-        return view('setup.index');
+        $requirements = [
+            'PHP 7.4' => [
+                'result' => PHP_VERSION_ID >= 70400,
+                'description' => null,
+            ],
+            'bcmath extension' => [
+                'result' => extension_loaded('bcmath'),
+                'description' => null,
+            ],
+            'ctype extension' => [
+                'result' => extension_loaded('ctype'),
+                'description' => null,
+            ],
+            'json extension' => [
+                'result' => extension_loaded('json'),
+                'description' => null,
+            ],
+            'mbstring extension' => [
+                'result' => extension_loaded('mbstring'),
+                'description' => null,
+            ],
+            'openssl extension' => [
+                'result' => extension_loaded('openssl'),
+                'description' => null,
+            ],
+            'pdo_mysql extension' => [
+                'result' => extension_loaded('pdo_mysql'),
+                'description' => null,
+            ],
+            'tokenizer extension' => [
+                'result' => extension_loaded('tokenizer'),
+                'description' => null,
+            ],
+            'xml extension' => [
+                'result' => extension_loaded('xml'),
+                'description' => null,
+            ],
+            'env_writable' => [
+                'result' => File::isWritable(base_path('.env')),
+                'description' => null,
+            ],
+            'storage_writable' => [
+                'result' => File::isWritable(storage_path()) && File::isWritable(storage_path('logs')),
+                'description' => null,
+            ],
+        ];
+        
+        return view('setup.index', compact('requirements'));
     }
 }

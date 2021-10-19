2808ebcb471ff9742de55f09a06215a9264b5d32
Justin Byrne <theskaterjesus@gmail.com>
2021-10-19 16:38:41 +0100

    Improved component attributes

--- resources/views/components/nav/responsive-link.blade.php
+++ resources/views/components/nav/responsive-link.blade.php
@@ -1,11 +1,9 @@
 @props(['active'])
 
-@php
-$classes = ($active ?? false)
-            ? 'block pl-3 pr-4 py-2 border-l-4 border-red-400 text-base font-medium text-red-700 bg-red-50 focus:outline-none focus:text-red-800 focus:bg-red-100 focus:border-red-700 transition duration-150 ease-in-out'
-            : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
-@endphp
-
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a {{ $attributes->class([
+        'block pl-3 pr-4 py-2 border-l-4 text-base font-medium focus:outline-none transition duration-150 ease-in-out',
+        'border-red-400 text-red-700 bg-red-50 focus:text-red-800 focus:bg-red-100 focus:border-red-700' => $active,
+        'border-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300' => ! $active,
+    ]) }}>
     {{ $slot }}
 </a>

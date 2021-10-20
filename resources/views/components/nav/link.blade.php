40ef7ee8a0e2050966f955b0e7511622bd3d00bc
Justin Byrne <theskaterjesus@gmail.com>
2021-10-20 19:27:58 +0100

    Fixed missing variable issue

--- resources/views/components/nav/link.blade.php
+++ resources/views/components/nav/link.blade.php
@@ -1,4 +1,4 @@
-@props(['active'])
+@props(['active' => false])
 
 <a {{ $attributes->class([
         'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out',
--- resources/views/components/nav/responsive-link.blade.php
+++ resources/views/components/nav/responsive-link.blade.php
@@ -1,4 +1,4 @@
-@props(['active'])
+@props(['active' => false])
 
 <a {{ $attributes->class([
         'block pl-3 pr-4 py-2 border-l-4 text-base font-medium focus:outline-none transition duration-150 ease-in-out',

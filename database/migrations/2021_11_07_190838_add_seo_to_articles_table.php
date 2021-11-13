141e4b09a4250e40da4d99e38a6f6eacb291ba40
Justin Byrne <theskaterjesus@gmail.com>
2021-11-13 11:54:30 +0000

    Made author mullable

--- database/migrations/2021_11_07_190838_add_seo_to_articles_table.php
+++ database/migrations/2021_11_07_190838_add_seo_to_articles_table.php
@@ -16,8 +16,7 @@ class AddSeoToArticlesTable extends Migration
         Schema::table('articles', function (Blueprint $table) {
             $table->string('keywords')->after('published_at');
             $table->string('description')->after('keywords');
-            $table->unsignedBigInteger('author_id')->after('description');
-
+            $table->unsignedBigInteger('author_id')->after('description')->nullable();
             $table->foreign('author_id')->references('id')->on('users');
         });
     }

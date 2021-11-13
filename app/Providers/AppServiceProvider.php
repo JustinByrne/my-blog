31d2c40386f003a8f1d992ae5c4aa133cb3165d2
Justin Byrne <theskaterjesus@gmail.com>
2021-11-13 15:37:09 +0000

    Moved settings to composer file This also includes a check to see if database is connected

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Setting;
 use Illuminate\Support\Facades\View;
+use App\View\Composers\SettingComposer;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,12 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::share('site_name', Setting::where('name', 'site_name')->first()->value);
-        View::share('tag_line', Setting::where('name', 'tag_line')->first()->value);
-        View::share('site_logo', Setting::where('name', 'site_logo')->first()->value);
-        View::share('facebook_url', Setting::where('name', 'facebook_url')->first()->value);
-        View::share('instagram_url', Setting::where('name', 'instagram_url')->first()->value);
-        View::share('github_url', Setting::where('name', 'github_url')->first()->value);
-        View::share('twitter_url', Setting::where('name', 'twitter_url')->first()->value);
+        View::composer('*', SettingComposer::class);
     }
 }
---
+++ app/View/Composers/SettingComposer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\View\Composers;
+
+use App\Models\Setting;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
+
+class SettingComposer
+{
+    public $settings;
+
+    public function __construct()
+    {
+        $this->settings = [
+            'site_name',
+            'tag_line',
+            'site_logo',
+            'facebook_url',
+            'instagram_url',
+            'github_url',
+            'twitter_url',
+        ];
+    }
+    
+    public function compose(View $view)
+    {
+        try {
+            DB::connection()->getPdo();
+        } catch (\Exception $e) {
+            foreach ($this->settings as $setting) {
+                View::share($setting, null);
+            }
+
+            return;
+        }
+        
+        foreach ($this->settings as $setting) {
+            View::share($setting, Setting::where('name', $setting)->first()->value);
+        }
+    }
+}

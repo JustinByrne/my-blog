c8b21ee536cb5d831203338c160b3f12dd358a24
Justin Byrne <JustinByrne@users.noreply.github.com>
2021-11-20 07:47:09 +0000

    Added nullable to SEO columns

--- database/migrations/2021_11_07_190838_add_seo_to_articles_table.php
+++ database/migrations/2021_11_07_190838_add_seo_to_articles_table.php
@@ -14,8 +14,8 @@ class AddSeoToArticlesTable extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->string('keywords')->after('published_at');
-            $table->string('description')->after('keywords');
+            $table->string('keywords')->after('published_at')->nullable();
+            $table->string('description')->after('keywords')->nullable();
             $table->unsignedBigInteger('author_id')->after('description')->nullable();
             $table->foreign('author_id')->references('id')->on('users');
         });

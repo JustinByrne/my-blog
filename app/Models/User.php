7d72ec581df0e7d9460a4ea4fe7308c375780fdb
Justin Byrne <theskaterjesus@gmail.com>
2021-11-20 16:30:00 +0000

    Fixed user article relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,6 @@ class User extends Authenticatable
 
     public function articles()
     {
-        return $this->hasMany(Article::class);
+        return $this->hasMany(Article::class, 'author_id');
     }
 }
